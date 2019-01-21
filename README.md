b5d713dfd914f05ac23b4408db43308888a8a54a
vahit <vahid.maani@gmail.com>
2019-01-21 10:40:10 +0330

    Now we can specify interval time.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # K8S cluster status 
 
-**usage:** `bash ./kube-status.sh CONTEXT NAMESPACE`
+- **usage:** `bash ./kube-status.sh CONTEXT NAMESPACE <INTERVAL>`
 
-**dependencies:** kubectl access, tmux
+- **dependencies:** kubectl access, tmux, watch 
 
-**Note:** I'm using the portrait monitor.
+- **Note:** I'm using the portrait monitor.
--- kube-status.sh
+++ kube-status.sh
@@ -4,19 +4,20 @@
 
 CONTEXT=${1}
 NAMESPACE="${2}"
+INTERVAL=${3:-2}
 [[ -z "${NAMESPACE}" || -z "${CONTEXT}" ]] && echo "usage: $(basename ${0}) CONTEXT NAMESPACE." && exit 1
 
 # Create new window
 tmux new-window -n "K8S status" "exec watch --no-title -d \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get pod --output custom-columns='NAME:.metadata.name,STATUS:.status.phase,RESTARTS:.status.containerStatuses[*].restartCount,NODE:.spec.nodeName' --no-headers | grep -v Succeeded"\"
 
 # Create new pane
-tmux split -v -p 21 "exec watch --no-title -d kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get hpa"
+tmux split -v -p 21 "exec watch --interval ${INTERVAL} --no-title -d kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get hpa"
 
 tmux select-pane -t 1
-tmux split -v -p 47 "exec watch --no-title -d \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" top pod | tail --lines +2 | sed 's/  */|/g' | column -s'|' -t\"
+tmux split -v -p 47 "exec watch --interval ${INTERVAL} --no-title -d \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" top pod | tail --lines +2 | sed 's/  */|/g' | column -s'|' -t\"
  # \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" top pod | tail --lines +2 | column\""
 
 # Back to the previuse pane
 # tmux select-pane -t 1
-tmux split -h -p 40 "exec watch --no-title -d \"kubectl --context ${CONTEXT} top node | tail --lines +2 | sed 's/  */ /g' | cut -f1,3,5 -d ' ' | sed 's/ /\t/g'\""
+tmux split -h -p 40 "exec watch --interval ${INTERVAL} --no-title -d \"kubectl --context ${CONTEXT} top node | tail --lines +2 | sed 's/  */ /g' | cut -f1,3,5 -d ' ' | sed 's/ /\t/g'\""
 
