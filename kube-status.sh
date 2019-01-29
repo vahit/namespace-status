e470cccbdeaee86b331bf37f108982080a625fb5
vahit <vahid.maani@gmail.com>
2019-01-29 17:01:32 +0330

    add ready or not ready status.

--- kube-status.sh
+++ kube-status.sh
@@ -8,7 +8,7 @@ INTERVAL=${3:-2}
 [[ -z "${NAMESPACE}" || -z "${CONTEXT}" ]] && echo "usage: $(basename ${0}) CONTEXT NAMESPACE." && exit 1
 
 # Create new window
-tmux new-window -n "K8S status" "exec watch --no-title -d \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get pod --output custom-columns='NAME:.metadata.name,STATUS:.status.phase,RESTARTS:.status.containerStatuses[*].restartCount,NODE:.spec.nodeName' --no-headers | grep -v Succeeded"\"
+tmux new-window -n "K8S status" "exec watch --no-title -d \"kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get pod --output custom-columns='NAME:.metadata.name,STATUS:.status.phase,RESTARTS:.status.containerStatuses[*].restartCount,READY:.status.containerStatuses[*].ready,NODE:.spec.nodeName' --no-headers | grep -v Succeeded"\"
 
 # Create new pane
 tmux split -v -p 21 "exec watch --interval ${INTERVAL} --no-title -d kubectl --context ${CONTEXT} --namespace "${NAMESPACE}" get hpa"
@@ -20,4 +20,3 @@ tmux split -v -p 47 "exec watch --interval ${INTERVAL} --no-title -d \"kubectl -
 # Back to the previuse pane
 # tmux select-pane -t 1
 tmux split -h -p 40 "exec watch --interval ${INTERVAL} --no-title -d \"kubectl --context ${CONTEXT} top node | tail --lines +2 | sed 's/  */ /g' | cut -f1,3,5 -d ' ' | sed 's/ /\t/g'\""
-
